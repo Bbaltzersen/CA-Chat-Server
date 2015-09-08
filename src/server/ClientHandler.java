c219a78ba22344a665234037b9114b74dce2cf6b
matyasfeher <fehermatyas17@gmail.com>
2015-09-08 22:30:05 +0200

    ProtocolStrings added...

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -3,6 +3,8 @@ package server;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,6 +19,7 @@ public class ClientHandler extends Thread {
     Socket socket;
     PrintWriter pw;
     Scanner sc;
+    Map users;
 
     ClientHandler(Socket socket) throws IOException {
         this.socket = socket;
@@ -27,7 +30,6 @@ public class ClientHandler extends Thread {
     public void run() {
         String input = sc.nextLine();
         String[] parts = input.split("#");
-        boolean connected = true;
         if (!parts[0].equals("USER#")) {
             try {
                 socket.close(); // closing the connection
@@ -35,8 +37,14 @@ public class ClientHandler extends Thread {
                 Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+        boolean connected = true;
         while (connected) {
             if (parts[0].equals("USER#")) {
+                users = new HashMap();
+                users.put(parts[0], parts[1]);
+
+            }else{
+                System.out.println("Apenado senior, no es un User!");
             }
         }
     }
---
+++ src/shared/ProtocolStrings.java
@@ -0,0 +1,16 @@
+package shared;
+
+/**
+ *
+ * @author Acer
+ */
+public class ProtocolStrings {
+    
+    public static final String USER = "USER#";
+    public static final String MSG = "MSG#";
+    public static final String STOP = "STOP#";
+    
+    public static final String USERLIST = "USERLIST#";
+    
+    
+}
