b53bfd8b6faef02703dea9e3bd776a91d9113ed4
benthehorn <benhorn13@googlemail.com>
2015-09-13 11:03:13 +0200

    50% of client tests work. wow.

--- test/client/BenClientTest.java
+++ test/client/BenClientTest.java
@@ -5,8 +5,11 @@
  */
 package client;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Observable;
 import java.util.Observer;
+import static org.hamcrest.CoreMatchers.is;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
@@ -18,7 +21,8 @@ import server.ClientHandler;
  * @author Ben
  */
 public class BenClientTest implements Observer {
-    
+        private PrintWriter output;
+
     public BenClientTest() {
     }
 
@@ -54,14 +58,14 @@ public class BenClientTest implements Observer {
      * Test of getList method, of class BenClient.
      */
     @Test
-    public void testGetList() {
+    public void testGetList() throws IOException {
         System.out.println("getList");
         BenClient instance = new BenClient();
-        String[] expResult = null;
+        instance.connect("localhost",7777,"Test4",this);
+        String[] expResult = instance.sUsers ;
+        
         String[] result = instance.getList();
         assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -70,14 +74,13 @@ public class BenClientTest implements Observer {
     @Test
     public void testSend() throws Exception {
         System.out.println("send");
-            ClientHandler ch;
-          BenClient client = new BenClient();
-    client.connect("localhost",7777,"Test2",this);
-    client.connect("localhost",7777,"Test3",this);
-        String msg = "MSG#Test2#Hello";
-        client.send(msg);
-        
-        assertEquals(expResult, ch.);
+            String msg = "MSG#Test2#Hello";
+          BenClient client2 = new BenClient();
+          BenClient client3= new BenClient();
+    client2.connect("localhost",7777,"Test2",this);
+    client3.connect("localhost",7777,"Test3",this);
+        client3.send(msg);
+        assertEquals(msg, client2.recieve());
         // TODO review the generated test code and remove the default call to fail.
     }
 
@@ -85,10 +88,12 @@ public class BenClientTest implements Observer {
      * Test of close method, of class BenClient.
      */
     @Test
-    public void testClose() {
+    public void testClose() throws IOException {
         System.out.println("close");
-        BenClient instance = new BenClient();
-        instance.close();
+       BenClient client = new BenClient();
+       client.connect("localhost",7777,"Test1",this);
+       client.close();
+        assertEquals(client, this);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -98,13 +103,14 @@ public class BenClientTest implements Observer {
      */
     @Test
     public void testRecieve() throws Exception {
-        System.out.println("recieve");
-        BenClient instance = new BenClient();
-        String expResult = "";
-        String result = instance.recieve();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+           System.out.println("send");
+            String msg = "USERLIST#Test2,Test3";
+          BenClient client2 = new BenClient();
+          BenClient client3= new BenClient();
+    client2.connect("localhost",7777,"Test2",this);
+    client3.connect("localhost",7777,"Test3",this);
+        
+        assertThat((client3.recieve()), is("USERLIST#Test2,Test3"));
     }
 
     @Override
